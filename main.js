3810cf643bc01a256ea9a7da99d0ed050cb8c271
kwangyong <dnflrhkddyd@naver.com>
2022-01-08 21:04:29 +0900

    JS : event delegation

--- main.js
+++ main.js
@@ -19,34 +19,26 @@ function onAdd() {
     input.focus();
 }
 
+let id = 0;
 function makeItem(text){
     const itemRow = document.createElement('li');
     itemRow.setAttribute('class', 'item__row');
+    itemRow.setAttribute('data-id', id);
 
-    const item = document.createElement('div');
-    item.setAttribute('class', 'item');
-
-    const name = document.createElement('span');
-    name.setAttribute('class', 'item__name');
-    name.innerText=text;
-
-    const deleteBtn = document.createElement('button');
-    deleteBtn.setAttribute('class', 'item__delete');
-    deleteBtn.innerHTML = '<i class="fas fa-trash-alt"></i>';
-    deleteBtn.addEventListener('click', ()=>{
-        items.removeChild(itemRow);
-    })
+    itemRow.innerHTML = `
+    <div class="item">
+        <span class="item__name">${text}</span>
+        <button class="item__delete">
+            <i class="fas fa-trash-alt" data-target-id=${id}></i>
+        </button>
+    </div>
+    <div class="item__divider"></div>
+    `;
     
-    const itemDivider = document.createElement('div');
-    itemDivider.setAttribute('class', 'item__divider');
+    id++;
 
-    itemRow.appendChild(item);
-    item.appendChild(name);
-    item.appendChild(deleteBtn);
-    itemRow.appendChild(itemDivider);
     return itemRow;
 }
-
 
 addBtn.addEventListener('click', () => {
     onAdd();
@@ -57,3 +49,12 @@ input.addEventListener('keypress', (event) => {
         onAdd();
     }
 });
+
+items.addEventListener('click', (event) =>{
+    const targetId = event.target.dataset.targetId;
+    if(targetId){
+        const toBeDeleted = document.querySelector(`.item__row[data-id="${targetId}"]`);
+        toBeDeleted.remove();
+    }
+    
+})
